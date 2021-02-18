274c2b39b64008757b0cb56ffe1239e1e7cd9f4d
Antoine <antoinea723@gmail.com>
2021-02-18 15:30:58 +0100

    Menu en jeu fonctionnel

--- UIProjetUnity/Assets/Scripts/ButtonBehavior.cs
+++ UIProjetUnity/Assets/Scripts/ButtonBehavior.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class ButtonBehavior : MonoBehaviour
 {
-   
+    [SerializeField] private Canvas gameMenu;
     public void Play()
     {
         SceneManager.LoadScene("SampleScene");
@@ -13,5 +13,17 @@ public class ButtonBehavior : MonoBehaviour
     {
         Application.Quit();
     }
-    
+
+    public void Resume()
+    {
+        gameMenu.gameObject.SetActive(false); //On désactive le canvas pour revenir en jeu
+        Time.timeScale = 1f; /*On redéfinit l'échelle de temps (vitesse à laquelle le temps passe)
+        Sur 1 pour que le jeu reprenne normalement, car dans le script gameMenu, lors de l'affichage du Canvas,
+        on a arrêté le défilement du jeu*/
+    }
+
+    public void ExitToHome()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
