d3a8d848ab614a875951c409b27de2566d58f6f9
Antoine <antoinea723@gmail.com>
2021-03-07 15:24:50 +0100

    Ajout derniers audios

--- UIProjetUnity/Assets/gameManager.cs
+++ UIProjetUnity/Assets/gameManager.cs
@@ -6,16 +6,9 @@ using UnityEngine;
 public class gameManager : MonoBehaviour
 {
     private bool key1;
-    private void Awake()
-    {
-        //Cursor.SetCursor(cursorTexture,hotSpot,cursor);   
-    }
+    [SerializeField] private AudioClip keyClip;
 
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private AudioSource source;
     // Update is called once per frame
     void Update()
     {
@@ -23,6 +16,7 @@ public class gameManager : MonoBehaviour
         {
             Instantiate(Resources.Load("key1"), new Vector3(-0.71f, 0.62f, 3.17f), Quaternion.identity).name="key1";
             key1 = true;
+            source.PlayOneShot(keyClip);
         }
         
     }
